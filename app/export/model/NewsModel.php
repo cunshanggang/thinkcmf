505808eb315505cd403f9db23db78ac48b844e9d
cunshanggangg <cunshanggangg@gmail.com>
2018-01-24 11:45:24 +0800

    The connection between model and table name.

---
+++ app/export/model/NewsModel.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/1/24
+ * Time: 11:38
+ */
+namespace app\export\model;
+use think\Model;
+class NewsModel extends Model {
+
+}
